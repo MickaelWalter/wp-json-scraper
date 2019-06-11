3baf6a4c18ea030363e0dfbf44f4ab7c4edc46bb
Mickaël Walter <8983311+MickaelWalter@users.noreply.github.com>
2019-06-11 23:11:05 +0200

    Bump version number for new release

--- WPJsonScraper.py
+++ WPJsonScraper.py
@@ -34,7 +34,7 @@ from lib.exceptions import NoWordpressApi, WordPressApiNotV2, \
 from lib.exporter import Exporter
 from lib.requestsession import RequestSession
 
-version = '0.3'
+version = '0.4'
 
 def main():
     parser = argparse.ArgumentParser(description=
