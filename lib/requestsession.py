cb8415d0acb5243daac55b2f22a4efcc9541038e
Mickaël Walter <8983311+MickaelWalter@users.noreply.github.com>
2019-06-11 21:18:29 +0200

    Updated copyright for 2019 and minor cosmetic improvement

--- lib/requestsession.py
+++ lib/requestsession.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2018 Mickaël "Kilawyn" Walter
+Copyright (c) 2018-2019 Mickaël "Kilawyn" Walter
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
